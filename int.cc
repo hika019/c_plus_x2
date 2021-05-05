304911ed07c8db52aeef4d40629be4d6312f8119
hika019 <ikazuti2001@gmail.com>
2021-05-05 16:56:02 +0900

    型推論

--- int.cc
+++ int.cc
@@ -1,10 +1,16 @@
 #include <iostream>
 
 int main(){
+  //read only
   const int x=5;
+
+  //定数
   constexpr int y=10;
+
+  auto s = "Hello";
 
   std::cout << x << '\n';
   std::cout << y << '\n';
+  std::cout << s << '\n';
   return 0;
 }
