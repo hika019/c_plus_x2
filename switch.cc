ab65a55044d54e90afde5a39adb74d1b43b25d5d
hika019 <ikazuti2001@gmail.com>
2021-05-05 18:43:21 +0900

    break追加で解決

--- switch.cc
+++ switch.cc
@@ -9,8 +9,10 @@ int main(){
         break;
       case 2:
         std::cout << "x:2" << std::endl;
+        break;
       case 3:
         std::cout << "x:3" << std::endl;
+        break;
     }
 
   }
